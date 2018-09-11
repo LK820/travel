98e1806ab8d4f9a7c10490c0fe339ec1acf8933f
LK <transelement@outlook.com>
2018-09-11 21:52:21 +0800

    icons

---
+++ src/page/home/components/icon.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="icons">
+        <swiper class="swipper" :options="swiperOption" >
+            <swiper-slide >
+                <div class="box">
+                    <div class="img_box" v-for="i of iconList">
+                        <img class="img-content" :src="i.img_url">
+                        <p>{{i.description}}</p>
+                    </div>
+                </div>
+            </swiper-slide>
+
+
+            <swiper-slide >
+                <div class="box">
+                    <div class="img_box" v-for="i of iconList">
+                        <img class="img-content" :src="i.img_url">
+                        <p>{{i.description}}</p>
+                    </div>
+
+                </div>
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "icon",
+        data (){
+            return{
+                swiperOption: {
+                    loop:true
+                },
+                iconList:[{
+                    id:'01',
+                    img_url:require("../../../assets/img/jar.png"),
+                    description:"出境游"
+                },{
+                    id:'01',
+                    img_url:require("../../../assets/img/cup.png"),
+                    description:"热门风景"
+                },{
+                    id:'01',
+                    img_url:require("../../../assets/img/knife.png"),
+                    description:"美食林"
+                },{
+                    id:'01',
+                    img_url:require("../../../assets/img/mw.png"),
+                    description:"超级巴士"
+                },{
+                    id:'01',
+                    img_url:require("../../../assets/img/eggbeater.png"),
+                    description:"攻略"
+                },{
+                    id:'01',
+                    img_url:require("../../../assets/img/trash.png"),
+                    description:"汽车票"
+                },{
+                    id:'01',
+                    img_url:require("../../../assets/img/rolling.png"),
+                    description:"国内游"
+                },{
+                    id:'01',
+                    img_url:require("../../../assets/img/urn.png"),
+                    description:"签证"
+                }]
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+.box{
+    display: flex;
+    height: 20vh;
+    flex-wrap: wrap;
+}
+.img_box{
+    width: 25%;
+    text-align: center;
+}
+.img-content{
+    width: 40%;
+    height: 40%;
+    margin-bottom: 5px;
+    margin-top: 15px;
+}
+</style>
--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -2,17 +2,20 @@
     <div>
         <homeheader></homeheader>
         <swiper></swiper>
+        <icon></icon>
     </div>
 </template>
 
 <script>
     import Header from './components/header'
     import Swiper from './components/swiper'
+    import Icon from './components/icon'
     export default {
         name: "home",
         components:{
             homeheader:Header,
-            swiper:Swiper
+            swiper:Swiper,
+            icon:Icon
         }
     }
 </script>
